4bbf08e5fe457a87a316cfe083c072146b04d05a
yliharsilamikko <yliharsilamikko@gmail.com>
2022-01-28 14:46:00 +0200

    commit

---
+++ 05_fanuc_controller_http_client/app.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const express = require('express');
+const PORT = process.env.PORT || 8081;
+let app = express();
+
+
+app.get("/robot/joint_values/", (req, res, next) => {
+  axios
+    .get("https://fanuc-robot-http-server.herokuapp.com/")
+    .then((robot_res) => {
+      const regexp = "Joint   [1-6]: *(-?.*)";
+      let joint_values = [];
+      let matches = robot_res.data.matchAll(regexp);
+      let count = 0;
+      for (const match of matches) {
+        count++;
+        if (count > 6) break;
+        const value = parseFloat(match[1]);
+        joint_values.push(value);
+      }
+      console.log(joint_values);
+      res.send(joint_values);
+    });
+});
+
+app.listen(PORT);
