bf9d75ad300ac45f3434be6098f6ff0d00b4319d
yliharsilamikko <yliharsilamikko@gmail.com>
2022-02-25 20:02:46 +0200

    commit

--- 10_fanuc_viewer/app.js
+++ 10_fanuc_viewer/app.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.static("node_modules/three/build"));
 
 app.listen(PORT);
 console.log("Listening port: ",PORT);
--- 10_fanuc_viewer/public/client.js
+++ 10_fanuc_viewer/public/client.js
@@ -1,3 +1,23 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer();
+
+const aspect = window.innerWidth / window.innerHeight;
+const camera = new THREE.PerspectiveCamera(75, aspect);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// renderer.c
+
+const render = ()=>{
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+};
+render();
+
+
 let socket = io("https://fanuc-wsserver.herokuapp.com/", {withCredentials: false});
 
 socket.on("joint_values", (joint_values) =>{
