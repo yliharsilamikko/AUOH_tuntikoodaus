58d0145233cff8017d68218fc89bdd6bf1db3aae
yliharsilamikko <yliharsilamikko@gmail.com>
2022-03-12 08:49:20 +0200

    commit

--- 10_fanuc_viewer/app.js
+++ 10_fanuc_viewer/app.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.static("node_modules/three/build"));
 app.use(express.static("node_modules/three/examples/jsm/controls"));
+app.use(express.static("node_modules/three/examples/jsm/loaders"));
+
 
 app.listen(PORT);
 console.log("Listening port: ",PORT);
--- 10_fanuc_viewer/public/client.js
+++ 10_fanuc_viewer/public/client.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
-import {OrbitControls} from "/OrbitControls.js"
+import {OrbitControls} from "/OrbitControls.js";
+import {STLLoader} from "/STLLoader.js";
 
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
@@ -35,6 +36,33 @@ let controls = new OrbitControls(camera, renderer.domElement);
    scene.add(mesh);
 }
 
+let default_material = new THREE.MeshPhongMaterial({
+    color: 0x0000ff
+});
+
+const stl_loader = new STLLoader();
+const load_stl = (url)=>{
+    return new Promise((resolve)=>{
+        stl_loader.load(url, resolve);
+    });
+};
+
+// robot
+{
+    //let base_geometry = await load_stl(...);
+    //let base_geometry = await load_stl(...);
+    //let base_geometry = await load_stl(...);
+
+
+    load_stl("./FANUC_R2000iA165F-STL/BASE.stl").then( (geometry)=>{
+        geometry.scale(0.001, 0.001, 0.001);
+        let mesh = new THREE.Mesh(geometry, default_material);
+        scene.add(mesh);
+    } );
+
+}
+
+
 const render = ()=>{
     renderer.render(scene, camera);
     controls.update();
@@ -46,6 +74,5 @@ render();
 let socket = io("https://fanuc-wsserver.herokuapp.com/", {withCredentials: false});
 
 socket.on("joint_values", (joint_values) =>{
-
-    console.log(joint_values);
+    //console.log(joint_values);
 });
