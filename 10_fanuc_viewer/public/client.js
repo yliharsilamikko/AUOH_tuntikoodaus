de492a27a3afcb35384a577cd70f350ade6730b2
yliharsilamikko <yliharsilamikko@gmail.com>
2022-03-12 09:49:12 +0200

    commit

--- 10_fanuc_viewer/public/client.js
+++ 10_fanuc_viewer/public/client.js
@@ -16,6 +16,15 @@ renderer.setClearColor(0xffffff);
 
 let controls = new OrbitControls(camera, renderer.domElement);
 
+// origo
+{
+    let geometry = new THREE.SphereGeometry(0.05);
+    let material = new THREE.MeshBasicMaterial({color: 0xff0000});
+    let mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+}
+
+
 // lightning
 {
     let light = new THREE.DirectionalLight(0xaaaaaa, 1.5);
@@ -25,6 +34,16 @@ let controls = new OrbitControls(camera, renderer.domElement);
     light.lookAt(scene.position);
     scene.add(light);
 }
+{
+    let light = new THREE.DirectionalLight(0xaaaaaa, 1.5);
+    light.position.x = -10;
+    light.position.y = -10;
+    light.position.z = 10;
+    light.lookAt(scene.position);
+    scene.add(light);
+}
+
+
 
 // floor
 {
@@ -48,19 +67,96 @@ const load_stl = (url)=>{
 };
 
 // robot
-{
-    //let base_geometry = await load_stl(...);
-    //let base_geometry = await load_stl(...);
-    //let base_geometry = await load_stl(...);
+let joints = [];
 
+const load_geometries = async ()=>{
 
-    load_stl("./FANUC_R2000iA165F-STL/BASE.stl").then( (geometry)=>{
+    {
+        let geometry = await load_stl("./FANUC_R2000iA165F-STL/BASE.stl");
         geometry.scale(0.001, 0.001, 0.001);
-        let mesh = new THREE.Mesh(geometry, default_material);
-        scene.add(mesh);
-    } );
+        joints.push(new THREE.Mesh(geometry, default_material));
+    }
+    {
+        let geometry = await load_stl("./FANUC_R2000iA165F-STL/J1-1.stl");
+        geometry.scale(0.001, 0.001, 0.001);
+        joints.push(new THREE.Mesh(geometry, default_material));
+    }
+    {
+        let geometry = await load_stl("./FANUC_R2000iA165F-STL/J2.stl");
+        geometry.scale(0.001, 0.001, 0.001);
+        joints.push(new THREE.Mesh(geometry, default_material));
+    }
+    {
+        let geometry = await load_stl("./FANUC_R2000iA165F-STL/J3.stl");
+        geometry.scale(0.001, 0.001, 0.001);
+        joints.push(new THREE.Mesh(geometry, default_material));
+    }
+    {
+        let geometry = await load_stl("./FANUC_R2000iA165F-STL/J4.stl");
+        geometry.scale(0.001, 0.001, 0.001);
+        joints.push(new THREE.Mesh(geometry, default_material));
+    }
+    {
+        let geometry = await load_stl("./FANUC_R2000iA165F-STL/J5.stl");
+        geometry.scale(0.001, 0.001, 0.001);
+        joints.push(new THREE.Mesh(geometry, default_material));
+    }
+};
 
-}
+// Nivelten origot ja pyörimisakselien suunnat
+// J1: [0, 282,0] Y
+// J2: [312, 670, -117]  Z
+// J3: [268.69, 1744.13, -196.85] Z
+// J4: [1315.19, 1969.13, 0.15] X
+// J5: [1548.69, 1969.13, 87.15] Z
+// J6: [1763.69, 1969.13, 20.47] X
+
+
+let offsets = [];
+load_geometries().then(()=>{
+
+    joints[1].geometry.translate(0, -0.282, 0);
+    joints[2].geometry.translate(-0.312, -0.670, 0.117);
+    joints[3].geometry.translate(-0.26869, -1.74413, 0.19685);
+    joints[4].geometry.translate(-1.31519, -1.96913, -0.00015);
+    joints[5].geometry.translate(-1.54869, -1.96913, -0.08715);
+
+    scene.add(joints[0]);
+    joints[0].rotation.set(THREE.Math.degToRad(90), 0, 0);
+
+    offsets.push(new THREE.Group());
+    offsets[0].position.set(0, 0.282,0);
+    joints[0].add(offsets[0]);
+    offsets[0].add(joints[1]);
+    
+    offsets.push(new THREE.Group());
+    offsets[1].position.set(-0.312, (-0.282)-(-0.670),0.117);
+    joints[0].add(offsets[0]);
+    offsets[0].add(joints[1]);
+    
+    offsets.push(new THREE.Group());
+    offsets[2].position.set(-0.312, (-0.282)-(-0.670),0.117);
+    joints[1].add(offsets[1]);
+    offsets[1].add(joints[2]);
+
+
+    joints[1].rotation.set(0,THREE.Math.degToRad(90),0);
+
+
+    // Math.degToRad
+    
+    //joints[0].add(joints[1]);
+    //joints[1].add(joints[2]);
+
+});
+
+// {
+//     load_stl("./FANUC_R2000iA165F-STL/BASE.stl").then( (geometry)=>{
+//         geometry.scale(0.001, 0.001, 0.001);
+//         let mesh = new THREE.Mesh(geometry, default_material);
+//         scene.add(mesh);
+//     } );
+// }
 
 
 const render = ()=>{
