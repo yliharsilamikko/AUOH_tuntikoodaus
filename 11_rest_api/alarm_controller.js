9eaf2c413352260289ec28469896f1e3c60c89c9
yliharsilamikko <yliharsilamikko@gmail.com>
2022-04-08 15:44:13 +0300

    commit

--- 11_rest_api/alarm_controller.js
+++ 11_rest_api/alarm_controller.js
@@ -1,16 +1,54 @@
 const alarm_model = require("./alarm_model");
 
+
+// CRUD
+
+//CREATE
+
 const api_post_alarm = (req, res) => {
   let model = alarm_model(req.body);
+  
   model
     .save()
     .then((model) => {
       res.send(model);
     })
     .catch((err) => {
-        res.status(500);
-        res.send(err.message);
+      res.status(500);
+      res.send(err.message);
     });
 };
 
+// READ
+
+const api_get_alarms = (req, res) => {
+    alarm_model.find({}).then((alarms)=>{
+        res.send(alarms);
+    });
+};
+
+const api_get_alarm = (req, res) => {
+    const id = req.params.id;
+    alarm_model.findById(id).then((alarm)=>{
+        res.send(alarm);
+    }).catch(()=>{
+        res.status(404);
+        res.send("not found");
+    });
+};
+
+
+const api_put_alarm = (req, res) => {
+    const id = req.params.id;
+    alarm_model.findByIdAndUpdate(id, req.body).then((alarm)=>{
+        res.send(alarm);
+    });
+};
+
+
 module.exports.api_post_alarm = api_post_alarm;
+module.exports.api_get_alarms = api_get_alarms;
+module.exports.api_get_alarm = api_get_alarm;
+module.exports.api_put_alarm = api_put_alarm;
+
+
--- 11_rest_api/app.js
+++ 11_rest_api/app.js
@@ -11,6 +11,9 @@ app.use(body_parser.json());
 // CREATE
 
 app.post("/api/alarm", alarm_controller.api_post_alarm);
+app.get("/api/alarms", alarm_controller.api_get_alarms);
+app.get("/api/alarm/:id", alarm_controller.api_get_alarm);
+app.put("/api/alarm/:id", alarm_controller.api_put_alarm);
 
 const db_uri = "mongodb+srv://db_user:HxK0dIDDkci6Y7la@cluster0.ganue.mongodb.net/alarm_db?retryWrites=true&w=majority";
 
