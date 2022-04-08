da72b9987de07092c656871bd2a83a83ccf8632c
yliharsilamikko <yliharsilamikko@gmail.com>
2022-04-08 15:23:04 +0300

    commit

--- 11_rest_api/alarm_controller.js
+++ 11_rest_api/alarm_controller.js
@@ -1,8 +1,16 @@
+const alarm_model = require("./alarm_model");
 
-
-const api_post_alarm = (req, res) =>{
-    console.log(req.body);
-    res.send("asdf");
-}
+const api_post_alarm = (req, res) => {
+  let model = alarm_model(req.body);
+  model
+    .save()
+    .then((model) => {
+      res.send(model);
+    })
+    .catch((err) => {
+        res.status(500);
+        res.send(err.message);
+    });
+};
 
 module.exports.api_post_alarm = api_post_alarm;
